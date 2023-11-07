a49af68874fdccc2f1e0974b21e802df9e6ef646
BuildTools <unconfigured@null.spigotmc.org>
2023-11-08 00:24:27 +0300

    up

---
+++ class/computers_fetch.php
@@ -0,0 +1,34 @@
+<?php  
+include("../inc/config.php");
+if ( $_SESSION['Oturum'] != 'true' ) { 
+ header("Location:login.php");
+}
+if(isset($_POST["id_data"]))  
+ {  
+      $query = "SELECT * FROM computers WHERE id='".$_POST["id_data"]."'";
+      $result = sqlsrv_query($con, $query);  
+	  while ($row = sqlsrv_fetch_array($result)) {
+		$data = array(
+    		"id"=>$row['id'],
+    		"users"=>$row['users'],
+    		"device_name"=>$row['device_name'],
+			"brand"=>$row['brand'],
+			"model"=>$row['model'],
+			"sn"=>$row['sn'],
+			"os"=>$row['os'],
+			"office"=>$row['office'],
+			"licence"=>$row['licence'],
+			"antivirus"=>$row['antivirus'],
+			"ad"=>$row['ad'],
+			"cpu"=>$row['cpu'],
+			"ram"=>$row['ram'],
+			"hdd"=>$row['hdd'],
+			"ip"=>$row['ip'],
+			"mac"=>$row['mac'],
+			"location"=>$row['location'],
+			"nots"=>$row['nots']
+    	);
+	}
+echo json_encode($data);
+ }  
+?>
--- computer.php
+++ computer.php
@@ -223,6 +223,136 @@ case "":
                     </div>
                 </div>
 			</form>		
+			
+			
+<form action="computer.php?process=update" method="post">
+		 <div class="modal fade" id="edit_modal" tabindex="-1" aria-labelledby="editmodaltitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-xl">
+                        <div class="modal-content">
+                            <div class="modal-header bg-transparent">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body px-sm-5 mx-50 pb-5">
+                                <h1 class="text-center mb-1" id="addNewCardTitle">Update</h1>
+								
+								<div class="row">
+
+									<div class="col-md-6">
+									
+											<label class="form-label" for="Users">Kullanıcı</label>
+											<select data-placeholder="Select a users..." name="users" class="form-select" id="users">
+											<option value="">Kullanıcı Yok</option>
+											<?php 
+												$departmanet_query = sqlsrv_query($con,"SELECT * FROM employee where hide='0'");
+												while($departmanet_row = sqlsrv_fetch_array($departmanet_query)){
+													echo '<option value="'.$departmanet_row["id"].'">'.$departmanet_row["name"].'</option>';
+												}									
+											?>
+											</select>									
+									
+											<label class="form-label" for="Device_name">Cihaz Adı</label>
+											<input type="text" id="device_name" class="form-control" name="device_name" placeholder="Cihaz Adı" required />
+
+											<label class="form-label" for="Brand">Marka</label>
+											<input type="text" id="brand" class="form-control" name="brand" placeholder="brand"  />
+
+											<label class="form-label" for="Model">Model</label>
+											<input type="text" id="model" class="form-control" name="model" placeholder="model"  />
+
+											<label class="form-label" for="Sn">SN</label>
+											<input type="text" id="sn" class="form-control" name="sn" placeholder="sn"  />
+											
+											<label class="form-label" for="Os">İşletim Sistemi</label>
+											<input type="text" id="os" class="form-control" name="os" placeholder="os"  />
+
+											<label class="form-label" for="Office">Office</label>
+											<input type="text" id="office" class="form-control" name="office" placeholder="office"  />
+											
+                                            <label class="form-label" for="Lisans">Lisans</label>
+                                            <select data-placeholder="Select a licence..." name="licence" class="select2-icons form-select" id="licence">
+                                                    <option value="0" data-icon="circle">Yok</option>
+                                                    <option value="1" data-icon="circle">Var</option>
+                                            </select>
+											
+                                     </div>
+									 
+									  <div class="col-md-6">
+
+                                            <label class="form-label" for="Antivirus">Antivirüs</label>
+                                            <select data-placeholder="Select a Antivirus..." name="antivirus" class="select2-icons form-select" id="antivirus">
+                                                    <option value="0" data-icon="circle">Yok</option>
+                                                    <option value="1" data-icon="circle">Var</option>
+                                            </select>
+											
+                                            <label class="form-label" for="Ad">Active Directory</label>
+                                            <select data-placeholder="Select a licence..." name="ad" class="select2-icons form-select" id="ad">
+                                                    <option value="0" data-icon="circle">Yok</option>
+                                                    <option value="1" data-icon="circle">Var</option>
+                                            </select>											
+											
+											<label class="form-label" for="Cpu">İşlemci</label>
+											<input type="text" id="cpu" class="form-control" name="cpu" placeholder="Cpu"  />
+											
+											<label class="form-label" for="Ram">Ram</label>
+											<input type="number" id="ram" class="form-control" name="ram" placeholder="Ram"  />
+
+											<label class="form-label" for="Hdd">Hdd</label>
+											<input type="number" id="hdd" class="form-control" name="hdd" placeholder="Hdd"  />
+											
+											<label class="form-label" for="Ip">İp</label>
+											<input type="text" id="ip" class="form-control" name="ip" placeholder="Ip"  />
+
+											<label class="form-label" for="Mac">Mac</label>
+											<input type="text" id="mac" class="form-control" name="mac" placeholder="Mac"  />
+											
+											<label class="form-label" for="Konumu">Konumu</label>
+											<input type="text" id="location" class="form-control" name="location" placeholder="Konumu"  />
+																					
+                                    </div>
+							
+											<div class="mb-1">
+													<label class="form-label" for="Not">Not</label>
+													<textarea name="nots" class="form-control" id="nots" rows="2" placeholder="Not"></textarea>
+											</div>
+							
+								</div>
+	
+                                    <div class="col-12 text-center">
+                                        <button type="submit" class="btn btn-primary me-1 mt-1">Update</button>
+                                        <button type="reset" class="btn btn-outline-secondary mt-1" data-bs-dismiss="modal" aria-label="Close">
+                                            Cancel
+                                        </button>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				<input type="hidden" name="id" id="id" required />
+			</form>				
+			
+		  <form action="computer.php?process=delete" method="post">
+			 <div class="modal fade" id="delete_modal" tabindex="-1" aria-labelledby="deletemodaltitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header bg-transparent">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body px-sm-5 mx-50 pb-5">
+                                    <div class="col-12 text-center">
+									<h1 class="text-center" id="deletemodaltitle">ID: <b id="id_delete_show"></b> Are you sure you want to delete this item?</h1>
+                                        <button type="submit" class="btn btn-danger me-1 mt-1">Delete</button>
+                                        <button type="reset" class="btn btn-outline-secondary mt-1" data-bs-dismiss="modal" aria-label="Close">
+                                            Cancel
+                                        </button>
+                                    </div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				<input type="hidden" name="id" id="id_delete" required />
+			</form>			
+			
+			
 <?php
 break;
 	
@@ -275,24 +405,15 @@ $edit_row1 = sqlsrv_fetch_array($edit_query1);
 sqlsrv_query($con,"UPDATE computers SET  tur='".$tur."', teslim_alma_tarih='".$teslim_alma_tarih."', vade_tarih='".$vade_tarih."', tutar='".$tutar."', doviz='".$doviz."' where id='".$id."' ");
 
 
-echo '<meta http-equiv="refresh" content="0;URL=firma.php?id='.$edit_row1["hash"].'">'; 
-
+echo '<meta http-equiv="refresh" content="0;URL=computer.php?id='.$edit_row1["hash"].'">'; 
 break;
 
 case "delete": 
-$id = $_GET['id'];
-$edit_query1 = sqlsrv_query($con, "select * computers WHERE id='".$id."'");
-$edit_row1 = sqlsrv_fetch_array($edit_query1);
-echo "Silmek İstiyormuzunuz ? <a href='firma.php?a=delete_onay&id=".$id."'>Onayla</a> -";
-echo " <a href='firma.php?id=".$edit_row1["hash"]."'>İptal</a>";
-break;
-
-case "delete_onay":
-$id = $_GET['id'];
-$edit_query1 = sqlsrv_query($con, "select * computers veri WHERE id='".$id."'");
-$edit_row1 = sqlsrv_fetch_array($edit_query1);
-sqlsrv_query($con,'DELETE computers veri where id="'.$id.'"'); 
-echo '<meta http-equiv="refresh" content="0;URL=firma.php?id='.$edit_row1["hash"].'">'; 
+$id = $_POST['id'];
+sqlsrv_query($con,"UPDATE computers SET hide='1' where id='".$id."' ");
+$process_id = $id;
+sqlsrv_query($con,"INSERT INTO logs (user_id,module,process_id,process) values ('".$_SESSION['UserID']."','".$_SERVER['REQUEST_URI']."','".$process_id."','delete')");
+echo '<meta http-equiv="refresh" content="0;URL=computer.php">';
 break;
 
 }
@@ -350,7 +471,55 @@ break;
         })
     </script>
 
-										
+<script>
+	
+			 
+		 
+
+	 $(document).ready(function(){  
+	      $(document).on('click', '.edit_modal', function(){  
+           var id_data = $(this).attr("id");  
+           $.ajax({  
+                url:"class/computers_fetch.php",  
+                method:"POST",  
+                data:{id_data:id_data},  
+                dataType:"json",  
+                success:function(data){  
+					 $('#id').val(data.id);
+                     $('#users').val(data.users);  
+                     $('#device_name').val(data.device_name);  
+					 $('#brand').val(data.brand);  
+                     $('#model').val(data.model); 
+					 $('#sn').val(data.sn);  
+                     $('#os').val(data.os); 
+					 $('#office').val(data.office);
+					 $('#licence').val(data.licence);
+					 $('#antivirus').val(data.antivirus);
+					 $('#ad').val(data.ad);
+					 $('#office').val(data.office);
+					  $('#cpu').val(data.cpu);
+					   $('#ram').val(data.ram);
+					    $('#hdd').val(data.hdd);
+						 $('#ip').val(data.ip);
+						  $('#mac').val(data.mac);
+						  $('#location').val(data.location);
+						  $('#nots').val(data.nots);
+                     $('#edit_modal').modal('show');  
+                }  
+           });  
+		 });
+		 
+	  $(document).on('click', '.delete_modal', function(){  
+           var id_data = $(this).attr("id");  
+		   $('#id_delete').val(id_data);
+		   $('#id_delete_show').text(id_data); 
+		   $('#delete_modal').modal('show'); 
+      });
+	  
+	  
+	 });  
+	</script>
+	
 <script>
         $(document).ready(function(){
             var dataTable = $('#empTable').DataTable({
@@ -383,7 +552,7 @@ break;
               '<a type="button" id="'+ data +'" class="dropdown-item delete-record delete_modal">' + feather.icons['trash-2'].toSvg({ class: 'me-50' }) +
               'Delete</a>' +
               '</div>' +
-              '</div> <a href="computers.php?process=edit&id='+ data +'" class="item-edit" title="Edit">'+ feather.icons['edit'].toSvg({ class: 'me-50' }) +'</a>' + '<a target="_blank" href="print/computers.php?id='+ data +'" class="item-edit" title="Print">'+ feather.icons['printer'].toSvg({ class: 'me-50' }) +'</a>' :
+              '</div> <a type="button" id="'+ data +'" class="item-edit edit_modal" title="Edit">'+ feather.icons['edit'].toSvg({ class: 'me-50' }) +'</a>' + '<a target="_blank" href="print/computers.php?id='+ data +'" class="item-edit" title="Print">'+ feather.icons['printer'].toSvg({ class: 'me-50' }) +'</a>' :
                 data;
 				}}
 				],
